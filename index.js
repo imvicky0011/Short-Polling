18c311bc95ccf6e70bdb6fcbce536d1f16682f37
imvicky0011 <kumarvikku0011@gmail.com>
2023-07-01 17:08:00 +0530

    added comments

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ app.get("/", (req, res) => {
     res.send("Welcome to the server")
 })
 
+
+//Whenever a new job is submitted, it will be added to the dictionary with a unique job ID, and the progess as it second key.
+//while registering the new job into the dictionary, you update the job by increasing the porgress of 10%, every 3 seconds.
+//to do this, we call a updateJob() function, it will a recursive function to trigger another function, every 3 seconds.
 app.post("/submit", (req, res) => {
     const jobId = `job:${Date.now()}`
     jobs[jobId] = 0
     updateJob(jobId, 0)
-    res.end("\n\n " + jobId + "\n\n")
+    res.send("\n\n " + jobId + "\n\n")
 })
 
 app.get("/checkstatus", (req, res) => {
     console.log(jobs[req.query.jobId])
-    res.end("\n\nJobStatus: " + jobs[req.query.jobId] + "%\n\n")
+    res.send("\n\nJobStatus: " + jobs[req.query.jobId] + "%\n\n")
 })
 
+
+//this will depict the updation in the job every 3 second, and console.log the progess everytime the update happens
+//it closely resembles how the short polling works,
+//at the same time, it resemebles: server and client are talking too much, and it might cause bottle neck within the givern network bandwidth
 function updateJob(jobId, prg) {
     jobs[jobId] = prg
     console.log(`updated ${jobId} to ${prg}`)
